d84862e2e855de1c0ccf07354bac065d733c4d6c
SaiSiddartha0906 <saisiddartha09062004@gmail.com>
2025-05-17 14:17:29 +0530

    Employee Payroll App

---
+++ src/main/java/com/example/employeepayroll/EmplolyeePayRollUc1Application.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplolyeePayRollUc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplolyeePayRollUc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeepayroll/EmplolyeePayRollUc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplolyeePayRollUc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
